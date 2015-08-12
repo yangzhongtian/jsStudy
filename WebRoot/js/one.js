1efb6b06c4c180cc04513d4fda061b1ee6e4aa98
yangzhongtian <858381638@qq.com>
2015-08-12 16:14:12 +0800

    javascript Dom  编程艺术  一书练习到41页；

--- WebRoot/js/one.js
+++ WebRoot/js/one.js
@@ -4,4 +4,80 @@
  * 
  */
  
+ // 转义字符 \
+ //alert("hello/'\"");
+ //
  
+ //数组声明
+// var arr = Array(5);
+// var arr  = Array();
+// var arr = Array();
+// var arr = Array();var aar = Array();
+// var arr = new Array();
+// var arr  = new Array();
+// console.info(arr); 
+// var arr = new Array("kd","hel","lds");
+// 
+// document.write(arr[0]);
+// 数组 可以包含任意类型
+//var arr = new Array();
+//arr[0] =1945;
+//arr[1]="string";
+//var name ="jlkds";
+//arr[2] =name;
+//arr[4] = 132;
+//var cd =alert(arr[3]);
+//console.info(cd);      //undefine
+//console.log(arr);
+//var arrs = new Array();
+//arrs[0] =arr;
+//document.write(arrs[0][1]);
+
+////数组可以在赋值时添加下标           关联数组
+//var arr= new Array();
+//arr["naem"] = "yang";
+//console.info(arr)        //[name:"yang"]
+//arr["age"] = 33;
+//alert(arr['age']);
+//console.info(arr);
+// 
+ 
+//对象
+//var lemonon = new Object();
+//lemonon.age =35;
+//lemonon.name ="wang";
+//console.info(lemonon);
+//var lemono = {name:'klsd',age:'llsl'}
+// console.log(lemono.name)
+ 
+// var arrs ={};
+// var arr ={name:'wang'}
+// arrs.arr=arr;
+// console.log(arrs.arr.name)
+ //alert(1+4*5);        //先乘除后加减
+ 
+// alert("10+20="+ (10+20));
+// alert("10+\"20\"="+(10+"20"));
+// 
+ 
+ /*if 语句   赋值 a = 3 成功 执行   a =false  a=0  失败     应该是先赋值在转换成 Boolean型判断*/
+ //alert(a=-1);
+		// var arr =new Array();
+		// 
+		// if(arr = arr[2]){
+		// 	alert("test");
+		// }
+		
+// 比较
+//var a =false;
+//var b ="";
+//if(a==b){                 a==b true          a === b  false 比较值并且比较类型
+//	alert(true);     
+//}
+//var a;
+//if(a= 0){
+//	alert(a);
+//}
+//alert(a);
+
+//alert(!1);            值为 false
