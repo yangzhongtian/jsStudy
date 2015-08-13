8acdbd44c23b185f9314473b628aa7be7ed84bc8
yangzhongtian <858381638@qq.com>
2015-08-13 22:25:22 +0800

    第七章

--- WebRoot/js/seven_two.js
+++ WebRoot/js/seven_two.js
@@ -1,3 +1,10 @@
+function insertAfter(newNode,targetNodes){
+	if(targetNodes.parentNode.lastNodes===targetNodes){
+		targetNodes.parentNode.appendChild(newNode)
+	}else{		
+		targetNodes.parentNode.insertBefore(newNode, targetNodes.nextSibling);
+	}
+}
 window.onload= function(){
 	var photos = document.createElement("IMG");
 
@@ -5,14 +12,24 @@ window.onload= function(){
 	photos.setAttribute("alt",'请选择要显示的图片');
 	photos.setAttribute("width","500px");
 	photos.setAttribute("height","300px");
+	photos.setAttribute('id','pho')
 	
 	document.body.appendChild(photos);
-	var description = document.createElement('p')
+	var description = document.createElement('p');
 	description.setAttribute('id',"description");
 	var text1 = document.createTextNode("图片描述");
-	photos.parentNode.insertBefore(description,photos);
+	description.appendChild(text1);
+	insertAfter(description,photos);
+	
+	var items =document.getElementsByTagName("a");
+	for(var i=0;i<items.length;i++){
+		items[i].onclick=function(){
+			var href = this.href;
+			document.getElementById('pho').setAttribute('src', href);
+			//document.getElementById("description").childNodes[0].nodeValue=this.title;
+			document.getElementById("description").firstChild.nodeValue=this.title;
+			return false;
+		}
+	}
 }
 
-function insertAfter(){
-	
-}
