ad76ae3a3740e4c46d3a69d13810c67c378e4c6d
yangzhongtian <858381638@qq.com>
2015-08-14 16:54:16 +0800

    第九章

--- WebRoot/js/nineThree.js
+++ WebRoot/js/nineThree.js
@@ -1,1 +1,25 @@
-//alert('nineThree Chapter')
+//alert('nineThree Chapter')Ext
+window.onload=function(){
+	var tables = document.getElementsByTagName('table');
+	for(var i =0;i<tables.length;i++){
+		//alert('hello');
+		var  trs =tables[i].getElementsByTagName('tr');
+		console.info(trs)
+		for (var z = 0; z < trs.length; z++) {
+			trs[z].id ='normal';
+			trs[z].onmouseover =function(){
+				//this.style.fontSize='20px';
+				//this.style.backgroundColor='blue';
+				this.style.fontWeight='bold'
+				this.setAttribute('id','ids')
+			}
+			trs[z].onmouseout=function(){
+				//this.style.fontSize='15px';
+				//this.style.backgroundColor='green';
+				this.style.fontWeight='normal',
+				this.setAttribute('id','normal')
+			}
+		}
+	}
+	
+}
